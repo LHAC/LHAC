86647a094dce24d0fd365cc55878b42c1cffa166
Xiaocheng Tang <xiaochengtang@dyn153230.cc.lehigh.edu>
2013-01-21 10:05:43 -0500

    Initial Commit

---
+++ LHAC_v1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  LHAC_v1
+//
+//  Created by Xiaocheng Tang on 1/21/13.
+//  Copyright (c) 2013 Xiaocheng Tang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
