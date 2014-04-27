d1387709c964b6f83143f83e5ee5b1dbcd29856b
mktal <xiaocheng.t@gmail.com>
2014-04-27 00:38:56 -0400

    updated demo file

--- src/LHAC_demo.m
+++ src/LHAC_demo.m
@@ -9,31 +9,15 @@ param.v = 2;
 param.e = 1e-6;
 % max iterations
 param.i = 1000;
-param.g = 6;
-param.l = 1;
 
 % lambda
-param.lmd = 0.7;
+param.lmd = 0.5;
 
+% use load for other files, e.g., ER_692
+fin = [pdir fname{1} ext];
+% read the data
+S = dlmread(fin);
 
-for ii=1:length(fname)
-	algs(1).name = 'LHAC';
-	algs(1).exps = [];
+W = LHAC(S, param.lmd, param);
 
-	tmp = struct();
-	tmp.name = fname{ii};
-	% day-hour-minute
-	fout = [pwd, '/outputs/', fname{ii}, '-lmd7-', datestr(now, 'dd-HH-MM')];
-	% use load for other files, e.g., ER_692
-	fin = [pdir fname{ii} ext];
-	% read the data
-	S = dlmread(fin);
-
-	[tmp.W, tmp.iter tmp.fval tmp.t tmp.normgs tmp.numActive] = LHAC(S, param.lmd, param);
-	tmp.W = sparse(tmp.W);
-	tmp.param = param;
-	algs(1).exps = [algs(1).exps tmp];
-
-	save(fout, 'algs');
-end
 
