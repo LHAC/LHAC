9b4d415e15fa322008dad19298c338069f5a420c
mktal <xiaocheng.t@gmail.com>
2014-05-04 12:57:18 -0400

    maintenances

---
+++ LHAC_v2/LHAC_v2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LHAC_v2
+//
+//  Created by Xiaocheng Tang on 5/4/14.
+//  Copyright (c) 2014 Xiaocheng Tang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
