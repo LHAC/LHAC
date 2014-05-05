47fd4f62f0249410f8b6d65b9ecf3fb262410f02
mktal <xiaocheng.t@gmail.com>
2014-05-05 00:26:43 -0400

    added MATLAB demo file and data file a9a

---
+++ src/demo_l1log.m
@@ -0,0 +1,19 @@
+clear;
+pdir = './';
+fname = {'a9a'};
+ext = '';
+
+% verbose
+param.v = 2;
+% optimal tol
+param.e = 1e-7;
+% lambda
+param.lmd = 1;
+% max iterations
+param.i = 3000;
+
+datafile = [pwd, '/', fname{1}, ext];
+
+[w iter fval t] = LHACl1log(datafile, param.lmd, param);
+
+
