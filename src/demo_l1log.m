c83ddb791dfa56176c3883091ac6dcc51455ee83
mktal <xiaocheng.t@gmail.com>
2014-11-07 00:20:42 -0500

    improved demo

--- src/demo_l1log.m
+++ src/demo_l1log.m
@@ -4,7 +4,7 @@ fname = {'a9a'};
 ext = '';
 
 % verbose
-param.v = 1;
+param.v = 0;
 % optimal tol
 param.e = 1e-7;
 % lambda
@@ -16,13 +16,13 @@ param.loss = 'log';
 param.cached = 0;
 param.filename = [pwd, '/', fname{1}, ext];
 
-
+fprintf('test 1 ---- reading data directly from file ---- 0%%...\n');
 [w] = lhac(param);
-fprintf('test 1 ---- reading data directly from file ---- finished!\n');
-fprintf('test 2 ---- passing data from memory in MATLAB ---- start!\n');
+fprintf('test 1 ---- reading data directly from file ---- 100%%!\n');
+fprintf('test 2 ---- passing data from memory in MATLAB ---- 0%%...\n');
 load a9a
-param.v = 0;
 [w, iter, fval, t] = lhac(param, data.y, full(data.X));
 fprintf('time = %.4e, iter = %d, optimal fval = %.4e\n', t(end), iter(end), fval(end));
+fprintf('test 2 ---- passing data from memory in MATLAB ---- 100%%!\n');
 
 
